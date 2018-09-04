de3f7d6e2237387e2ed628c1b9edbea6156d824d
Greg Goltsov <gregory@goltsov.info>
2018-09-04 16:05:54 +0100

    Fixed package.el

--- .spacemacs.d/layers/personal/packages.el
+++ .spacemacs.d/layers/personal/packages.el
@@ -4,14 +4,14 @@
       )
 
 (defun personal/post-init-evil ()
+  (setq-default
+   evil-escape-key-sequence "ii"
+   evil-escape-delay 0.3
 
-  (setq-default evil-escape-key-sequence "ii")
-  (setq-default evil-escape-delay 0.3)
-
-  (setq evil-emacs-state-cursor '("red" box))
-  (setq evil-normal-state-cursor '("green" box))
-  (setq evil-visual-state-cursor '("orange" box))
-  (setq evil-insert-state-cursor '("red" bar))
-  (setq evil-replace-state-cursor '("red" bar))
-  (setq evil-operator-state-cursor '("red" hollow))
-  )
+   evil-emacs-state-cursor '("red" box)
+   evil-normal-state-cursor '("green" box)
+   evil-visual-state-cursor '("orange" box)
+   evil-insert-state-cursor '("red" bar)
+   evil-replace-state-cursor '("red" bar)
+   evil-operator-state-cursor '("red" hollow)
+  ))
