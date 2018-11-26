3d62574f1c5a94c858a253afc91a45dd0fa203df
Greg Goltsov <gregory@goltsov.info>
2018-11-26 17:04:08 +0000

    Update config.el

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -2,3 +2,10 @@
 
 ;; Place your private configuration here
 (setq doom-font (font-spec :family "Consolas" :size 16))
+
+(map! (:after helm-files
+        :map (helm-find-files-map helm-read-file-map)
+        "C-l"      #'helm-execute-persistent-action
+        "C-h"      #'helm-find-files-up-one-level
+        )
+      )
