b21588d4cebbd32f9258e9f925bff5b5d0f38601
Greg Goltsov <gregory@goltsov.info>
2019-03-03 18:23:18 +0000

    Added org config

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -11,44 +11,44 @@
         "C-l"      #'helm-execute-persistent-action
         "C-h"      #'helm-find-files-up-one-level)
 
-      ;; Add easy way to find
-      (:map override
-        "M-f" #'swiper-helm)  ;; can also be called via SPC / b
+  ;; Add easy way to find
+  (:map override
+    "M-f" #'swiper-helm)  ;; can also be called via SPC / b
 
-      (:when (featurep! :feature evil)
-        :n "j" #'evil-next-visual-line
-        :n "k" #'evil-previous-visual-line)
+  (:when (featurep! :feature evil)
+    :n "j" #'evil-next-visual-line
+    :n "k" #'evil-previous-visual-line)
 
-      (:map comint-mode-map
-        "C-k"      #'comint-previous-input
-        "C-j"      #'comint-next-input
-        "M-k"      #'comint-previous-prompt
-        "M-j"      #'comint-next-prompt
-        "C-l"      #'helm-comint-input-ring)
-      
-      ;; FIXME: probably better done by using evil-collection-prodigy better
-      ;; https://github.com/emacs-evil/evil-collection/blob/master/evil-collection-prodigy.el
-      (:after prodigy
-        (:map prodigy-mode-map
-          "q" 'quit-window
+  (:map comint-mode-map
+    "C-k"      #'comint-previous-input
+    "C-j"      #'comint-next-input
+    "M-k"      #'comint-previous-prompt
+    "M-j"      #'comint-next-prompt
+    "C-l"      #'helm-comint-input-ring)
 
-          "j" 'prodigy-next
-          "k" 'prodigy-prev
-          "gg" 'prodigy-first
-          "G" 'prodigy-last
+  ;; FIXME: probably better done by using evil-collection-prodigy better
+  ;; https://github.com/emacs-evil/evil-collection/blob/master/evil-collection-prodigy.el
+  (:after prodigy
+    (:map prodigy-mode-map
+      "q" 'quit-window
 
-          "Jm" 'prodigy-jump-magit
-          "Jd" 'prodigy-jump-dired))
+      "j" 'prodigy-next
+      "k" 'prodigy-prev
+      "gg" 'prodigy-first
+      "G" 'prodigy-last
 
-      ;; LEADER-based shortcuts
-      :leader
-      (:desc "toggle" :prefix "t"
-        :desc "Line wrap" :n "l" #'toggle-truncate-lines)
+      "Jm" 'prodigy-jump-magit
+      "Jd" 'prodigy-jump-dired))
 
-      (:prefix ("a" . "apps")
-          :desc "Prodigy" "p"  #'prodigy
-          :desc "Deer"    "r"  #'deer
-          :desc "Ranger"  "R"  #'ranger))
+  ;; LEADER-based shortcuts
+  :leader
+  (:desc "toggle" :prefix "t"
+    :desc "Line wrap" :n "l" #'toggle-truncate-lines)
+
+  (:prefix ("a" . "apps")
+    :desc "Prodigy" "p"  #'prodigy
+    :desc "Deer"    "r"  #'deer
+    :desc "Ranger"  "R"  #'ranger))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -71,7 +71,19 @@
     :stop-signal 'sigkill
     :kill-process-buffer-on-stop t
     :ready-message "Use Control-C to stop this server"
-    :init (lambda () (pyvenv-activate "/path/to/venv")))
+    :init (lambda () (pyvenv-activate "/path/to/venv"))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Org config
+
+(setq org-directory "~/Dropbox/org")
+(setq org-archive-location (concat org-directory "/archive.org::* Projects"))
+; http://doc.endlessparentheses.com/Var/org-archive-location.html
+(setq org-refile-targets
+  '(("inbox.org" :maxlevel . 1)
+    ("projects.org" :maxlevel . 1)
+    ("people.org" :maxlevel . 1)
+    ("areas.org" :maxlevel . 1)))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -79,7 +91,7 @@
 (after! evil-escape
   (setq evil-escape-key-sequence "ii"))
 
-(after! shx
+(after! shx shell
   (shx-global-mode 1))  ; toggle shx-mode on globally
 
 
@@ -92,6 +104,7 @@
 ;; TODO: add IS_MAC etc
 (setq doom-font (font-spec :family "Fira Code" :size 16))
 (global-set-key (kbd "A-3") '(lambda () (interactive) (insert "#")))
+
 
 (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
 (add-to-list 'default-frame-alist '(ns-appearance . dark))
