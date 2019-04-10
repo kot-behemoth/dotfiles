d5eccbb9fd86048c389786ae11517aa5ffd81183
Greg Goltsov <gregory@goltsov.info>
2019-04-10 14:56:49 +0100

    Update +hydras.el

--- .doom.d/+hydras.el
+++ .doom.d/+hydras.el
@@ -188,46 +188,48 @@ _vr_ reset      ^^                       ^^                 ^^
   ("M-r" lsp-restart-workspace)
   ("S" lsp-shutdown-workspace))
 
-(defhydra dh-hydra-markdown-mode (:hint nil)
-  "
-Formatting        C-c C-s    _s_: bold          _e_: italic     _b_: blockquote   _p_: pre-formatted    _c_: code
 
-Headings          C-c C-t    _h_: automatic     _1_: h1         _2_: h2           _3_: h3               _4_: h4
+;; "
+;; Formatting        C-c C-s    _s_: bold          _e_: italic     _b_: blockquote   _p_: pre-formatted    _c_: code
+;; Headings          C-c C-t    _h_: automatic     _1_: h1         _2_: h2           _3_: h3               _4_: h4
+;; Lists             C-c C-x    _m_: insert item
+;; Demote/Promote    C-c C-x    _l_: promote       _r_: demote     _u_: move up      _d_: move down
+;; Links, footnotes  C-c C-a    _L_: link          _U_: uri        _F_: footnote     _W_: wiki-link
+;; "
 
-Lists             C-c C-x    _m_: insert item
+(defhydra hydra-markdown-mode (:color blue)
+  "Markdown editing"
 
-Demote/Promote    C-c C-x    _l_: promote       _r_: demote     _u_: move up      _d_: move down
+  ("s" markdown-insert-bold "Bold" :column "Formatting")
+  ("e" markdown-insert-italic "Italic")
+  ("b" markdown-insert-blockquote "Blockquote" :color blue)
+  ("p" markdown-insert-pre "Pre-formatted" :color blue)
+  ("c" markdown-insert-gfm-code-block "Code block")
+  ("C" markdown-insert-code "Inline code")
 
-Links, footnotes  C-c C-a    _L_: link          _U_: uri        _F_: footnote     _W_: wiki-link      _R_: reference
+  ("h" markdown-insert-header-dwim "Automatic heading" :column "Headings")
+  ("1" markdown-insert-header-atx-1 "h1")
+  ("2" markdown-insert-header-atx-2 "h2")
+  ("3" markdown-insert-header-atx-3 "h3")
+  ("4" markdown-insert-header-atx-4 "h4")
 
-"
+  ("m" markdown-insert-list-item "Insert item" :column "Lists")
 
-  ("s" markdown-insert-bold)
-  ("e" markdown-insert-italic)
-  ("b" markdown-insert-blockquote :color blue)
-  ("p" markdown-insert-pre :color blue)
-  ("c" markdown-insert-code)
+  ("l" markdown-promote "Promote" :column "Demote/Promote")
+  ("r" markdown-demote "Demote")
+  ("d" markdown-move-down "Move up")
+  ("u" markdown-move-up "Move down")
 
-  ("h" markdown-insert-header-dwim)
-  ("1" markdown-insert-header-atx-1)
-  ("2" markdown-insert-header-atx-2)
-  ("3" markdown-insert-header-atx-3)
-  ("4" markdown-insert-header-atx-4)
+  ("L" markdown-insert-link "Link" :color blue :column "Links/Footnotes")
+  ("U" markdown-insert-uri "URI" :color blue)
+  ("F" markdown-insert-footnote "Foortnote" :color blue)
+  ("W" markdown-insert-wiki-link "Wiki-link" :color blue))
 
-  ("m" markdown-insert-list-item)
+(map!
+  :localleader
+  :map markdown-mode-map
+  "," #'hydra-markdown-mode/body)
 
-  ("l" markdown-promote)
-  ("r" markdown-demote)
-  ("d" markdown-move-down)
-  ("u" markdown-move-up)
-
-  ("L" markdown-insert-link :color blue)
-  ("U" markdown-insert-uri :color blue)
-  ("F" markdown-insert-footnote :color blue)
-  ("W" markdown-insert-wiki-link :color blue)
-  ("R" markdown-insert-reference-link-dwim :color blue))
-
-;; (global-set-key [f9] 'dh-hydra-markdown-mode/body)
 
 (defhydra hydra-para (:color blue)
     "
@@ -258,7 +260,7 @@ Links, footnotes  C-c C-a    _L_: link          _U_: uri        _F_: footnote   
   (:desc "P.A.R.A." :n "P" #'hydra-para/body))
 
 ;; Git-timemachine
-(defhydra hydra-git-timemachine          
+(defhydra hydra-git-timemachine
     (:color red
      :body-pre (unless (bound-and-true-p git-timemachine-mode)
                  (call-interactively 'git-timemachine))
